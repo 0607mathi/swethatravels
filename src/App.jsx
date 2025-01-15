905d6954f109027f1f3cbba6c4c57c97965181cc
0607mathi <mathiyarasu.m0607@gmail.com>
2025-01-15 21:56:46 +0530

    nav bar completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ function App() {
   return (
     <>
       <div className="app-container">
-        <div className="header br">
+        <div className="header">
           <h2>Sri Swetha Travels</h2>
-          <div className="nav-bar br">
+          <div className="nav-bar">
             <ol>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
-              <li><a href=""></a></li>
+              <li><a href="">Home</a></li>
+              <li><a href="">About us</a></li>
+              <li><a href="">Services</a></li>
+              <li><a href="">Contact</a></li>
+              <li><a href="">Enquiery</a></li>
             </ol>
           </div>
         </div>
