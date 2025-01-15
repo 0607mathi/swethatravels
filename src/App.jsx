5c67a22c2d59316aa92bff61a083c0f4ff986596
0607mathi <mathiyarasu.m0607@gmail.com>
2025-01-15 20:18:52 +0530

    creating a app for my own travels

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <div className="app-container">
+        <div className="header br">
+          <h2>Sri Swetha Travels</h2>
+          <div className="nav-bar br">
+            <ol>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
