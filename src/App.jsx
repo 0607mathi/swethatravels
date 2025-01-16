19c6133eef0258189ef829c2755c93f5d5290366
0607mathi <mathiyarasu.m0607@gmail.com>
2025-01-16 14:54:21 +0530

    banner section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from 'react';
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false, 
+    });
+  }, []);
+
   return (
     <>
       <div className="app-container">
         {/* Header */}
         <div className="header">
-          <h2>Sri Swetha Travels</h2>
+          <h2>Swetha Tours & Travels</h2>
           <div className="nav-bar">
             <ol>
               <li><a href="">Home</a></li>
@@ -19,12 +30,17 @@ function App() {
         {/* Banner */}
         <div className="banner-section">
           
-          <div className="text-area">
-            <span className="color-text">Welcome to</span> <br />
-            Sri Swetha Travels
+          <div className="text-area" data-aos="fade-right">
+            <span className="color-text">Welcome</span> to <br />
+            Swetha <span className="color-text">Tours & Travels...!</span> <br />
+            <span className='sentance'><span className='color-text'>your Gateway to</span> Unforgettable Adventures !</span>
           </div>
 
-          <img src="innova-banner-2.jpg" alt="" />
+          <img src="Road trip-rafiki.svg" data-aos="fade-left" alt="" className='logo-image-travel' />
+        </div>
+        {/* Home */}
+        <div className="home-section">
+          <p>Discover the world like never before with expertly crafted travel experiences. Whether you're seeking serene escapes, thrilling adventures, or cultural explorations, we’ve got the perfect trip for you.</p>
         </div>
       </div>
     </>
