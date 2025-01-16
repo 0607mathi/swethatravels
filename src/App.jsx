e02b219ad0a1d310c8064829466ca40bfd2537b9
0607mathi <mathiyarasu.m0607@gmail.com>
2025-01-16 17:40:42 +0530

    Service section completed

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ function App() {
           <div className="nav-bar">
             <ol>
               <li><a href="">Home</a></li>
-              <li><a href="">About us</a></li>
               <li><a href="">Services</a></li>
+              <li><a href="">Booking</a></li>
               <li><a href="">Contact</a></li>
               <li><a href="">Enquiery</a></li>
             </ol>
@@ -46,7 +46,65 @@ function App() {
             <p className='' data-aos="fade-left">Discover the world like never before with expertly crafted travel experiences. Whether you're seeking serene escapes, thrilling adventures, or cultural explorations, we’ve got the perfect trip for you.</p>
           </div>
         </div>
-        {/* About */}
+        {/* plan content */}
+        <div className='plan-content' data-aos="fade-down">
+            <h3>Plan your Next Journey Today..!</h3>
+            <p>Let’s make your travel dreams come true. Browse our tours, customize your package, or get in touch for a consultation.</p>
+            <button><a href="#">Book your trip</a></button>
+        </div>
+        {/* Services */}
+        <div className="service-section">
+          <div className='headName' data-aos="fade-down"><span>Our Services</span></div>
+          <div className="ser-box">
+            <div className="box" data-aos="fade-right">
+              <img src="Study abroad-pana.svg" className='' alt="" />
+              <div className=''>
+                <h3>Expert Guides</h3>
+                <span>Knowledgeable guides to enrich your journey.</span>
+              </div>
+            </div>
+
+            <div className="box"data-aos="zoom-in">
+              <img src="Pina colada-cuate.svg" className='' alt="" />
+              <div className=''>
+                <h3>Seamless Planning</h3>
+                <span>Leave the details to us for a stress-free vacation.</span>
+              </div>
+            </div>
+
+            <div className="box"data-aos="fade-left">
+              <img src="Service 24_7-pana.svg" className='' alt="" />
+              <div className=''>
+                <h3>24/7 Support</h3>
+                <span>We’re here to assist you anytime, anywhere</span>
+              </div>
+            </div>
+
+            <div className="box"data-aos="fade-right">
+              <img src="Road trip-amico.svg" className='' alt="" />
+              <div className=''>
+                <h3>Favorite Vehicle</h3>
+                <span>Choose your Favorite Vechical for your memorable trip.</span>
+              </div>
+            </div>
+
+            <div className="box"data-aos="zoom-in">
+              <img src="Living room-amico.svg" className='' alt="" />
+              <div className=''>
+                <h3>Tailored Experiences</h3>
+                <span>Personalized itineraries designed to suit your preferences.</span>
+              </div>
+            </div>
+
+            <div className="box"data-aos="fade-left">
+              <img src="cruise-rafiki.svg" className='' alt="" />
+              <div className=''>
+                <h3>Cruise Bookings</h3>
+                <span>Sail through breathtaking oceans and rivers with our exclusive cruise packages for families, couples, and groups.</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
