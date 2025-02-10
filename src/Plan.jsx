8e38b4ad63f8772e75b3420b1aae28447de68143
0607mathi <mathiyarasu.m0607@gmail.com>
2025-02-10 14:35:16 +0530

    plan booking button

--- src/Plan.jsx
+++ src/Plan.jsx
@@ -6,7 +6,7 @@ export const Plan = () => {
          <div className='plan-content' data-aos="fade-down">
             <h3>Plan your Next Journey Today..!</h3>
             <p>Let’s make your travel dreams come true. Browse our tours, customize your package, or get in touch for a consultation.</p>
-            <button><a href="#">Book your trip</a></button>
+            <button><a href="#booking">Book your trip</a></button>
         </div>
     </>
   )
